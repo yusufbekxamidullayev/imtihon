4371460fbabedef009e1d0cfd2514e35251cb87b
yusufbekxamidullayev <yusufbekxamidullayev37@gmail.com>
2025-08-10 10:18:58 +0500

    adding

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const toogle = document.getElementById("toogle")
+const page = document.getElementById("page")
+const yopish = document.getElementById("yopish")
+
+
+toogle.addEventListener("click", () => {
+    page.classList.add("page-hide")
+})
+yopish.addEventListener("click" , () => {
+    menus.classList.add("menus-pide")
+})
