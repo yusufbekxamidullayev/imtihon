e9c143133a4d5289201644bed33a8f9f00a72c9c
yusufbekxamidullayev <yusufbekxamidullayev37@gmail.com>
2025-08-11 18:35:45 +0500

    adding

--- js/main.js
+++ js/main.js
@@ -5,7 +5,28 @@ const yopish = document.getElementById("yopish")
 
 toogle.addEventListener("click", () => {
     page.classList.add("page-hide")
+    page.classList.add("page-durition")
 })
-yopish.addEventListener("click" , () => {
-    menus.classList.add("menus-pide")
+yopish.addEventListener("click", () => {
+    page.classList.remove("page-hide")
+    page.classList.add("page-durition")
+})
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
 })
